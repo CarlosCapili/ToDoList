a968fd05c4dd0328945b1be01e95b396f342edc8
CarlosCapili <carloscapili21@gmail.com>
2021-08-27 16:10:07 -0400

    Implemented adding a task to UI

--- src/createNewTask.js
+++ src/createNewTask.js
@@ -1,8 +1,17 @@
 export default function createNewTask(title, desc, dueDate) {
     const task = document.createElement("div");
     task.classList.add("task");
-    task.textContent = title;
 
+    const layoutHTML = `
+        <input type="checkbox">
+        <div class="taskTitle"><p>${title}</p></div>
+        <div class="taskDate"><p>${dueDate}</p></div>
+        <svg id="trash" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+        </svg>`;
+
+    task.innerHTML = layoutHTML;
     const list = document.querySelector(".list");
     list.appendChild(task);
 }
