dbcd922df02918485a8fa43bfbffed10776bc060
CarlosCapili <carloscapili21@gmail.com>
2021-08-31 18:44:05 -0400

    A lot of updates to UI

--- src/index.js
+++ src/index.js
@@ -2,36 +2,58 @@ import Task from './TaskClass';
 
 let taskArr = [];
 
-//Use a module later!!! call it main
-const saveBtn = document.querySelector("#save");
-saveBtn.addEventListener("click", (e) => {
-    const taskTitle = document.querySelector("#title").value;
-    if (taskTitle !== "" && taskTitle.trim() !== "") {
-        let taskIndex = findTask(taskTitle);
-        if (taskIndex < 0) {
-            collectFormData();
-        } else {
-            alert("Task updated and saved");
-            editTaskInfo(taskIndex);
-            updateTaskUI(taskIndex);
-        }
-        let form = document.querySelector(".create-task");
-        form.reset();
-    }
-
-});
-
-//Select Task, notes, or projects
-//Default should be tasks
-const taskBtn = document.getElementById("task");
-const notesBtn = document.getElementById("notes");
-const projBtn = document.getElementById("projects");
-
-
+//Default UI
+loadCreateTask();
 
 const list = document.querySelector(".list");
-list.addEventListener("click", handleTask);
 
+//Load home, project, or notes
+const homeBtn = document.getElementById("home");
+const projBtn = document.getElementById("projects");
+const noteBtn = document.getElementById("notes");
+const homeNum = document.getElementById("homeNum");
+const projNum = document.getElementById("projNum");
+const noteNum = document.getElementById("noteNum");
+
+homeBtn.addEventListener("click", loadHome);
+projBtn.addEventListener("click", loadProj);
+noteBtn.addEventListener("click", loadNote);
+
+function loadHome() {
+    list.innerHTML = "";
+    taskArr.forEach(task => {
+        createNewTask(task.getTitle(), task.getDesc(), task.getDueDate());
+    });
+}
+
+function loadProj() {
+    list.innerHTML = "No project selected";
+}
+
+function loadNote() {
+    list.innerHTML = "No notes";
+}
+
+//Update the amount of items in home, proj, and notes
+function updateHomeNum() {
+    if (taskArr.length == 0) {
+        homeNum.textContent = "";
+    } else {
+        homeNum.textContent = taskArr.length;
+    }
+    console.log("HOME NUM UPDATED!");
+}
+
+function updateProjNum() {
+    
+}
+
+function updateNoteNum() {
+
+}
+
+//Task Handlers
+list.addEventListener("click", handleTask);
 function handleTask(e) {
     console.log(e.target);
 
@@ -100,13 +122,14 @@ function addTask(title, desc, dueDate) {
   const newTask = new Task(title, desc, dueDate);
   taskArr.push(newTask);
   console.log(taskArr);
+  updateHomeNum();
 }
 
 function deleteTask(e) {
     let taskIndex = findTask(e);
-    // console.log(`taskindex is ${taskIndex}`);
     taskArr.splice(taskIndex,1);
     console.log(taskArr);
+    updateHomeNum();
 }
 
 function deleteTaskUI(e) {
@@ -117,6 +140,7 @@ function findTask(title) {
     return taskArr.findIndex(task => task.title === title);
 }
 
+//UI Creation of task, project, and note 
 function createNewTask(title, desc, dueDate) {
     const task = document.createElement("div");
 
@@ -134,9 +158,111 @@ function createNewTask(title, desc, dueDate) {
         </svg>`;
 
     task.innerHTML = layoutHTML;
-    list.appendChild(task);  
+    list.appendChild(task); 
 }
 
+function createNewProj() {
+
+}
+
+function createNewNote() {
+
+}
+
+//Add horizontal bar
+const addContainer = document.querySelector(".add-option");
+const addBtns = addContainer.querySelectorAll(".option");
+
+//highlights active class
+addBtns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        let current = addContainer.querySelector(".active");
+        current.classList.remove("active");
+        e.target.className += " active";
+        loadCreateScreens(e)
+    });
+});
+
+//Vertical menu bar
+const menuContainer = document.querySelector("#left");
+const menuBtns = menuContainer.querySelectorAll(".item");
+
+//highlights active menu
+menuBtns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        let current = menuContainer.querySelector(".active");
+        current.classList.remove("active");
+        if (e.target.classList.contains("num")) {
+            e.target.parentNode.className += " active";
+        } else {
+            e.target.className += " active";
+        }
+    });
+});
+
+function loadCreateScreens(e) {
+    console.log(e.target.id);
+    if(e.target.id === "add-task") {
+        loadCreateTask();
+    } else if (e.target.id === "add-proj") {
+        loadCreateProj();
+    } else if (e.target.id === "add-note") {
+        loadCreateNote();
+    }
+}
+
+// Task, project, and note creation menu
+function loadCreateTask() {
+    const formTag = document.querySelector(".create-task");
+    formTag.innerHTML = `
+    <div>
+        <p>Due Date:</p>
+        <input id="date" type="date">
+    </div>
+    <input type="text" id="title" placeholder="Title" required>
+    <textarea id="description" placeholder="Description"></textarea>
+    <button type="button" id="save">Save</button>
+    `;
+
+    //Adds to list
+    const saveBtn = document.querySelector("#save");
+    saveBtn.addEventListener("click", (e) => {
+        console.log("THIS DOES NOT WORK AFTER!!");
+        const taskTitle = document.querySelector("#title").value;
+        if (taskTitle !== "" && taskTitle.trim() !== "") {
+            let taskIndex = findTask(taskTitle);
+            if (taskIndex < 0) {
+                collectFormData();
+            } else {
+                alert("Task updated and saved");
+                editTaskInfo(taskIndex);
+                updateTaskUI(taskIndex);
+            }
+            let form = document.querySelector(".create-task");
+            form.reset();
+        }
+    });
+}
+
+function loadCreateProj() {
+    const formTag = document.querySelector(".create-task");
+    formTag.innerHTML = `
+    <input type="text" id="title" placeholder="Project Name" required>
+    <textarea id="description" placeholder="Description"></textarea>
+    <button type="button" id="save">Save</button>
+    `;
+}
+
+function loadCreateNote() {
+    const formTag = document.querySelector(".create-task");
+    formTag.innerHTML = `
+    <input type="text" id="title" placeholder="Note Name" required>
+    <textarea id="description" placeholder="Description"></textarea>
+    <button type="button" id="save">Save</button>
+    `;
+}
+
+// Local storage functions
 function saveList() {
     let tasks = [];
 
